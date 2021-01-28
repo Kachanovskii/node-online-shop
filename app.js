37564070a2b184f56dbbd408e37ffbc133e803c9
Kachanovskii <vadimskyi1@gmail.com>
2021-01-28 18:06:58 +0200

    changes in app.js

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('working1')
+const express = require('express');
+
+
+const shopRoutes = require('./routes/shopRoutesr')
+
+const PORT = 3000;
+const app = express();
+
+app.use(shopRoutes)
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
---
+++ routes/shopRoutesr.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get("/",(req, res, next)=>{
+    res.send('<h1>Home page</h1>')
+})
+router.get("/about",(req, res, next)=>{
+    res.send('<h1>About page</h1>')
+})
+router.get("/vacancies",(req, res, next)=>{
+    res.send('<h1>Vacancies page</h1>')
+})
+router.get("/careers",(req, res, next)=>{
+    res.send('<h1>Careers page</h1>')
+})
+router.get("/careers",(req, res, next)=>{
+    res.send('<h1>Careers page</h1>')
+})
+router.get("/resources",(req, res, next)=>{
+    res.send('<h1>Resources page</h1>')
+})
+
+module.exports = router
